e1ca66a5d01fd2d5363bcc0ab4524564c65907f8
sediri-code <63328434+sediri-code@users.noreply.github.com>
2023-05-05 11:27:17 +0200

    added send_email function

--- mailer.py
+++ mailer.py
@@ -4,6 +4,22 @@ def send_email(to, subject, body, sender_email, sender_password):
     
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
+
+    
     server.login(sender_email, sender_password)
+
+    message = f'Subject: {subject}\n\n{body}'
+
     
-    
+    server.sendmail(sender_email, to, message)
+
+    server.close()
+
+
+to = 'recipient@example.com'
+subject = 'Test email'
+body = 'Hello, this is a test email!'
+sender_email = 'sender@gmail.com'
+sender_password = 'sender_password'
+
+send_email(to, subject, body, sender_email, sender_password)
