83cbc16f8dae72ec4f467477f491fdd252c78bdb
sediri-code <63328434+sediri-code@users.noreply.github.com>
2023-05-05 11:22:07 +0200

    first commit

--- mailer.py
+++ mailer.py
@@ -4,3 +4,6 @@ def send_email(to, subject, body, sender_email, sender_password):
     
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
+    server.login(sender_email, sender_password)
+    
+    
